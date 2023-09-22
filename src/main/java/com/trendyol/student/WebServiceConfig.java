6d664dc73b59bad2d6141b9550dfb910eda0f9ff
doruk.su <doruk.su@trendyol.com>
2023-09-22 20:25:47 +0300

    Extra informational comments added for rokiees

--- src/main/java/com/trendyol/student/WebServiceConfig.java
+++ src/main/java/com/trendyol/student/WebServiceConfig.java
@@ -13,6 +13,32 @@ import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+/*
+*
+* Author: Doruk S. 20.09.2023
+* Contact: doruk.su@trendyol.com
+*
+* WebServiceConfig sınıfı Spring Boot uygulamanızda SOAP web servisleri oluşturmak için bir konfigürasyon dosyasıdır.
+* @EnableWs ve @Configuration anotasyonları ile Spring'e bu sınıfın bir web servis konfigürasyonu olduğu belirtilir.
+*
+* @EnableWs: Spring WS'nin özelliklerini etkinleştirir.
+*
+* @Configuration: Bu sınıfın Spring konfigürasyon sınıfı olduğunu belirtir.
+*
+* messageDispatcherServlet(ApplicationContext applicationContext) : Bu bean, SOAP mesajlarını işlemek için kullanılan MessageDispatcherServlet'i yapılandırır.
+* Aynı zamanda bu servlet'in hangi URL kalıplarını işleyeceğini de belirler ("/ws/*")
+*
+* wsdl11Definition.setPortTypeName("StudentsPort"): Port tipini belirtir.
+wsdl11Definition.setLocationUri("/ws"): Bu servisin nerede olacağını belirtir.
+wsdl11Definition.setTargetNamespace(...): Hedef namespace'i belirtir.
+wsdl11Definition.setSchema(studentsSchema): Kullanacağınız şema. Bu şema genellikle XSD (XML Schema Definition) dosyasından gelir.
+
+*   Bir SOAP isteği geldiğinde, MessageDispatcherServlet devreye girer.
+    MessageDispatcherServlet uygulama bağlamını (ApplicationContext) kontrol eder ve uygun web servis endpointini bulur.
+    Sonrasında DefaultWsdl11Definition SOAP mesajını işler ve ilgili servise yönlendirir.
+    İlgili servis XSD şemasına uygun olup olmadığını kontrol eder. Eğer uygunsuz ise hata fırlatılır.
+* */
+
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
